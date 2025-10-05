f0d45d831ac2b8669afad8cbeddac0b20bfcca9b
Dextron12 <48815181+Dextron12@users.noreply.github.com>
2025-10-06 00:54:39 +1100

    Fixed CMakeLists for ftm library inclusion

---
+++ include/core/Error.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Dextron12 on 5/10/2025.
+//
+
+#ifndef DEXIUM_ERROR_HPP
+#define DEXIUM_ERROR_HPP
+
+
+
+class Error {
+
+};
+
+
+
+#endif //DEXIUM_ERROR_HPP
---
+++ src/core/Error.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dextron12 on 5/10/2025.
+//
+
+#include "../../include/core/Error.hpp"
