1a67b3257b77f592f8cd44d8e7f05ca6511a0166
Dextron12 <48815181+Dextron12@users.noreply.github.com>
2025-10-06 02:34:22 +1100

    Updated TraceLog to use CXX20 *print* instead of fprintf

--- include/core/Error.hpp
+++ include/core/Error.hpp
@@ -5,12 +5,28 @@
 #ifndef DEXIUM_ERROR_HPP
 #define DEXIUM_ERROR_HPP
 
+#include <fmt/core.h>
+#include <fmt/color.h>
 
 
-class Error {
 
-};
+namespace Dexium {
 
+    enum LogLevel {
+        LOG_TRACE = 1,      //< TRACE: debug or tracking messages
+        LOG_DEBUG = 2,      //< DEBUG: messages only output in debug mode
+        LOG_INFO = 3,       //< INFO: general engine state information
+        LOG_WARNING = 4,    //< WARNING: minor failure of engine, or incorrect usage of function/class
+        LOG_ERROR = 5,      //< ERROR: major failure, affected data likely lost(but engine/application can still operate without lost data)
+        LOG_FATAL = 6,      //< FATAL-ERROR: resource/state could not load, application will likely fail without this data (likely through a seg fault). In rel mode, will call abort() after dumping a logFile
+        LOG_NONE = 0        //< Disable/Enable all logging (engine-wide)
+    };
+
+    template <typename... Args>
+    void TraceLog(LogLevel level, fmt::format_string<Args...> fmtStr, Args&&... args);
+
+
+}
 
 
 #endif //DEXIUM_ERROR_HPP
--- src/core/Error.cpp
+++ src/core/Error.cpp
@@ -2,4 +2,46 @@
 // Created by Dextron12 on 5/10/2025.
 //
 
-#include "../../include/core/Error.hpp"
+#include <core/Error.hpp>
+
+
+template<typename... Args>
+void Dexium::TraceLog(LogLevel level, fmt::format_string<Args...> fmtStr, Args &&... args) {
+    fmt::color col;
+    std::string prefix;
+    switch (level) {
+        case LOG_TRACE:
+            prefix = "[TRACE] ";
+            col = fmt::color::white;
+            break;
+        case LOG_DEBUG:
+            prefix = "[DEBUG] ";
+            col = fmt::color::lime_green;
+            break;
+        case LOG_INFO:
+            prefix = "[INFO] ";
+            col = fmt::color::white;
+            break;
+        case LOG_WARNING:
+            prefix = "[WARNING] ";
+            col = fmt::color::yellow;
+            break;
+        case LOG_ERROR:
+            prefix = "[ERROR] ";
+            col = fmt::color::red;
+            break;
+        case LOG_FATAL:
+            prefix = "[FATAL] ";
+            col = fmt::color::purple;
+            break;
+        case LOG_NONE:
+            // Flag to enable/disable errorlogging functionality
+            // Needs a global state bool stored in engine for reference
+            break;
+    }
+
+    // Depending on configurations will, this func can output log to terminal, file, or the debug shell overlay.
+    // The debug shell overlay will require ImGui and hence will fallback to terminal or logFile if the lib isnt included
+
+    fmt::print(fmt::fg(col), "{}\n", fmt::format(prefix + fmtStr, std::forward<Args>(args)...));
+}
