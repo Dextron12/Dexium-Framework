7746cb71129c907502e445b2f1ce9510d45d5751
Dextron12 <48815181+Dextron12@users.noreply.github.com>
2026-02-21 21:19:41 +1100

    Amended gitignore to ignore old_target files

---
+++ include/core/BitwiseFlag.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ethan on 10/2/26.
+//
+
+#ifndef DEXIUM_BITWISEFLAG_HPP
+#define DEXIUM_BITWISEFLAG_HPP
+
+#endif //DEXIUM_BITWISEFLAG_HPP
---
+++ include/core/Error.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ethan on 10/2/26.
+//
+
+#ifndef DEXIUM_ERROR_HPP
+#define DEXIUM_ERROR_HPP
+
+#endif //DEXIUM_ERROR_HPP
